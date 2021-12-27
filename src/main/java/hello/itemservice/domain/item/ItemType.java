6bd71a0d74cadccd3a808a9b8b506b0ef2f19326
creatong <jgh3482@gmail.com>
2021-12-27 22:55:14 +0900

    thymeleaf를 이용한 form 만들기5-select box, radio button 추가

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -3,6 +3,10 @@ package hello.itemservice.domain.item;
 public enum ItemType {
     Book("도서"), Food("음식"), ETC("기타");
 
+    public String getDescription() {
+        return description;
+    }
+
     private  final String description;
 
     ItemType(String description) {
